09e4598d41b4291db8b099790e351f626ab050ac
hftamayo <hftamayo@gmail.com>
2023-05-12 15:46:43 -0600

    task component files part2

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,15 @@
 import "./Task.css";
 
+const STATUS = "PLANNED";
+
 export default function Task({ title }) {
-  return <div className="task">{title}</div>;
+  return (
+    <div className="task">
+      <div>{title}</div>
+      <div className="bottomWrapper">
+        <div></div>
+        <div className={classNames('status', STATUS)}>{STATUS}</div>
+      </div>
+    </div>
+  );
 }
